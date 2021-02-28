cfb14e5f5c8517edf70639ba9c045266334376fa
Shravankumar Bejugam <sbejugam@berkadia.com>
2021-02-28 16:25:33 +0530

    modified alphanum regex

--- utils/knowledge-base.js
+++ utils/knowledge-base.js
@@ -1,7 +1,7 @@
 module.exports = { // removed g flag for normal case...
     monthsEq: '(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|(Nov|Dec)(?:ember)?)',
     monthsRegex: /(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|(Nov|Dec)(?:ember)?)/i,
-    monthsRegexGlobal:/(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|(Nov|Dec)(?:ember)?)/ig,
+    monthsRegexGlobal: /(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|(Nov|Dec)(?:ember)?)/ig,
     words: /words/i,
     dates: /dates/i,
     normal: /normal/i,
@@ -16,7 +16,7 @@ module.exports = { // removed g flag for normal case...
     strictAlphabets: /^[a-z]+$/i,
     strictSpecialChars: /^[^a-z0-9]+$/i,
     alphaNumeric: /^(?=.*[a-z])(?=.*[0-9])[a-z0-9]+$/i,
-    alphaNumericWithSpecialChars: /^(?=.*[a-z])(?=.*[0-9])[a-z0-9-.\/!@#$%^&*\(\).~\`\\s]+$/i,
+    alphaNumericWithSpecialChars: /^(?=.*[a-z])(?=.*[0-9])[a-z0-9\W]+$/i,
     amounts: /^[0-9.,\-\+]+$/,
     alphaSpecialChars: /^[a-z-.\/!@#$%^&*\(\)\s.~`]+$/i,
     digitsSpecialChars: /^[0-9-.\/!@#$%^&*\(\).~\`\\s]+$/
